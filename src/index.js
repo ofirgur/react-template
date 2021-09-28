d4b435713f617dada9118082e6531b0a9c382b02
Ofir <ofirgu@payoneer.com>
2021-09-28 11:26:06 +0300

    add react router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 
 import App from './app';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+    <BrowserRouter basename="/calendar">
+        <Link to="/today">Link Today</Link> {/* renders <a href="/calendar/today"></a> */}
+        <Route path="/today" component={() => <div>Route Today 1 #</div>} />
+        <App />
+    </BrowserRouter>, 
+    document.getElementById('app')
+);
+
+// TO DO
+
+// 1. npm run build & webpack configuration
+// 2. react-router-dom refresh
