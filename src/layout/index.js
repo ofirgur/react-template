781345d6fd45bbdb5c77be1a39678aefa5bbf2c3
Ofir <ofirgu@payoneer.com>
2021-10-01 21:24:55 +0300

    update

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
 
 const Layout = () => {
     return (
-        <h1>App Layout</h1>
+        <Switch>
+          <Route exact path="/" component={() => (<div><h1>App Layout</h1><Link to="/example">link to /example</Link></div>)} />
+          <Route path="/example" component={() => (<div><h1>Examle</h1><Link to="/">back to /</Link></div>)} />
+        </Switch>
       );
 };
 
