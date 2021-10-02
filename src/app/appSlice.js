ef77275e80a47a5550c63677ba0824acfd6c2f08
Ofir <ofirgu@payoneer.com>
2021-10-02 16:06:45 +0300

    update  appSlice

--- src/app/appSlice.js
+++ src/app/appSlice.js
@@ -1,27 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { LOCATION_CHANGE } from 'connected-react-router';
 
-const initialState = {
-  value: 0,
-};
-
 export const appSlice = createSlice({
   name: 'app',
-  initialState,
+  initialState: {},
   reducers: {
-    increment: (state) => {
+    initialize: (state, action) => {
       // Redux Toolkit allows us to write "mutating" logic in reducers. It
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
-      state.value += 1
-    },
-    decrement: (state) => {
-      state.value -= 1
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload
-    },
+      state.data = action.payload
+    }
   },
   extraReducers: (builder) => {
     builder
@@ -35,6 +25,6 @@ export const appSlice = createSlice({
 });
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = appSlice.actions;
+export const { initialize } = appSlice.actions;
 
 export default appSlice.reducer;
