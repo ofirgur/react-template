183b269655745fd51cd3e0449af673ff12699ade
Ofir <ofirgu@payoneer.com>
2021-10-03 13:07:01 +0300

    update

--- src/index.js
+++ src/index.js
@@ -22,16 +22,18 @@ ReactDOM.render(
 // TO DO
 
 // 1. npm run build & webpack configuration
-// 2. react-router-dom refresh // done
+// 2. react-router-dom refresh // *****done
 // 3. server side rendering ?
-// 4. recoil // done
-// 5. react hooks deep dive // now! (custom hooks) // done
+// 4. recoil // *****done
+// 5. react hooks deep dive // now! (custom hooks) // *****done
 // 6. webpack deep div
 // 7. react debugging
 // 8. promises & async await
 // 9. react state deep dive
 // 10. linter & eslint-plugin-react-hooks 
 // 11. build url
-// 12. mocking
+// 12. mocking // *****done
 // 13. deeplinks
 // 14. eligibility
+// 15. __DEV__ flag for mock
+// 16. icons (where from?)
