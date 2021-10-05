964ed65e6b9eed7fa2689f26db5034634f7c7e91
Ofir <ofirgu@payoneer.com>
2021-10-05 16:16:20 +0300

    update

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,8 +1,20 @@
 class i18n {
     constructor() {
         this.initialize = this.initialize.bind(this);
+        this.getLocale = this.getLocale.bind(this);
+        this.setLocale = this.setLocale.bind(this);
+        this.loadNamespace = this.loadNamespace.bind(this);
+        this.loadResource = this.loadResource.bind(this);
         this.locale = 'en';
         this.namespaces = {};
+    }
+
+    getLocale() {
+        return this.locale;
+    }
+
+    setLocale(locale) {
+        this.locale = locale;
     }
 
     initialize(namespaces, locale) {
@@ -16,6 +28,16 @@ class i18n {
 
             return resources ? resources[key] : '';
         };
+    };
+
+    loadNamespace(name, namespace) {
+        this.namespaces[name] = namespace;
+    };
+
+    loadResource(name, resources) {
+        if(this.namespaces[name]) {
+            this.namespaces[name].resources = resources;
+        }
     };
 }
 
--- src/layout/index.js
+++ src/layout/index.js
@@ -7,13 +7,15 @@ const t = i18n.translateFrom('app');
 const Layout = () => {
     console.log('locale: ', i18n.locale);
     console.log('translate: ', t('HelloWorld.Text'));
-    
+    i18n.setLocale('en');
+    console.log('locale: ', i18n.locale);
+    console.log('translate: ', t('HelloWorld.Text'));
     return (
         <Switch>
           <Route exact path="/" component={() => (<div><h1>App Layout</h1><Link to="/example">link to /example</Link></div>)} />
           <Route path="/example" component={() => (<div><h1>Examle</h1><Link to="/">back to /</Link></div>)} />
         </Switch>
-      );
+    );
 };
 
 export default Layout;
