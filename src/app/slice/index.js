43df71e38f66fee1ed02ecf4ab96d8dbc451a8f2
Ofir <ofirgu@payoneer.com>
2021-10-13 08:54:25 +0300

    update app

--- src/app/slice/index.js
+++ src/app/slice/index.js
@@ -3,23 +3,8 @@ import { LOCATION_CHANGE } from 'connected-react-router';
 
 export const appSlice = createSlice({
   name: 'app',
-  initialState: {
-    user: {
-      locale: 'en',
-      currency: 'USD'
-    }
-  },
-  reducers: {
-    setUser: (state, action) => {
-      state.user = action.payload;
-    },
-    setUserLocale: (state, action) => {
-      state.user.locale = action.payload;
-    },
-    setUserCurrency: (state, action) => {
-      state.user.currency = action.payload;
-    }
-  },
+  initialState: {},
+  reducers: {},
   extraReducers: (builder) => {
     builder
     .addDefaultCase((state, action) => {
@@ -31,14 +16,8 @@ export const appSlice = createSlice({
   }
 });
 
-export const { setUser, setUserLocale, setUserCurrency } = appSlice.actions;
+export const {  } = appSlice.actions;
 
 export default appSlice.reducer;
 
-export const types = {
-  GET_USER: 'GET_USER'
-};
-
-export const getUserLocale = state => state.app.user.locale;
-export const getUserCurrency = state => state.app.user.currency;
 
