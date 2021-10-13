9c6ef5bb3b33ef31937cd31347646c4854f5fb2a
Ofir <ofirgu@payoneer.com>
2021-10-13 16:21:14 +0300

    update

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,34 +2,15 @@ import namespaces from './namespaces';
 
 class i18n {
     constructor() {
-        this.initialize = this.initialize.bind(this);
         this.translateFrom = this.translateFrom.bind(this);
-
-        this.getLocale = this.getLocale.bind(this);
-        this.setLocale = this.setLocale.bind(this);
-
-        this.locale = this.locale = 'en';
-        this.namespaces = namespaces;
     }
-
-    initialize(locale) {
-        this.locale = locale;
-    };
 
     translateFrom(namespace) {
         return locale => key => {
-            const resources = this.namespaces[namespace]?.resources[locale];
+            const resources = namespaces[namespace]?.resources[locale];
 
             return resources ? resources[key] : '';
         };
-    };
-
-    getLocale() {
-        return this.locale;
-    };
-
-    setLocale(locale) {
-        this.locale = locale;
     };
 }
 
