2261fc54116f663964413a464e716dd48514ff42
Ofir <ofirgu@payoneer.com>
2021-10-18 19:56:23 +0300

    update

--- src/siteMap/eligibility/index.js
+++ src/siteMap/eligibility/index.js
@@ -1,9 +1,61 @@
-export const isEligibleModule = user => module => {
-    if(!user || !module) return;
+export const isEligibleModule = eligibility => module => {
+    if(!eligibility || !module) return;
 
-    const { eligibility } = user;
     const { static, key } = module;
-    if(!eligibility || !key) return;
 
-    return static || eligibility[module.key];
+    return static || eligibility[key];
+};
+
+export const mapToEligibleChildRoutes = eligibility => route => {
+    if(!route) return;
+
+    const { childRoutes } = route;
+    if(!childRoutes) return route;
+    
+    return {                        
+        ...route,
+        childRoutes: childRoutes.filter(isEligibleModule(eligibility))
+    };
+};
+
+export const filterByEligibleChildRoutes = eligibility => route => {
+    if(!route) return;
+    
+    const { childRoutes } = route;
+    if(!childRoutes) return;
+
+    return childRoutes.length > 0 || isEligibleModule(eligibility)(route);
+};
+
+export const normalizeRoutesTree = (routes, parent) => {
+    if(!routes) return;
+
+    const route = {
+        ...routes,
+        ...buildRouteDetails(routes, parent)
+    };
+
+    const { childRoutes } = route;
+    if(!childRoutes) return route;
+
+    return {
+        ...route,
+        childRoutes: childRoutes.map(r => normalizeRoutesTree(r, route))
+    };
+};
+
+export const setEligibleRoutes = eligibility => routes => {
+    if(!routes) return;
+    
+    const { childRoutes } = routes;
+    if(!childRoutes) return;
+
+    const eRoutes = {
+        ...routes,
+        childRoutes: childRoutes
+            .map(mapToEligibleChildRoutes(eligibility))
+            .filter(filterByEligibleChildRoutes(eligibility))
+    };
+    
+    return normalizeRoutesTree(eRoutes);
 };
