6f7f742da31bfd8547fe581e659a44f6a0947497
Ofir <ofirgu@payoneer.com>
2021-10-20 12:13:28 +0300

    getEligibleRoute

--- src/siteMap/eligibility/index.js
+++ src/siteMap/eligibility/index.js
@@ -112,3 +112,7 @@ export const searchRouteFallback = (routes, pathname) => {
     return searchRouteFallback(routes, parentPathname);
 };
 
+export const getEligibleRoute = (routes, search) => {
+    return searchRoute(routes, search);
+};
+
