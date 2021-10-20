312583bf9c4af54ad76f57298c916d4e3532db9b
Ofir <ofirgu@payoneer.com>
2021-10-20 11:16:07 +0300

    update useLocation

--- src/app/index.js
+++ src/app/index.js
@@ -1,26 +1,19 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useLocation } from 'react-router';
 
 import { getUser } from '../user/http';
 import { types } from '../user/slice';
 import { isLoading } from '../redux-http/slice';
-import { setCurrentRoute } from '../siteMap/slice'; 
 
 import Layout from '../layout';
 
 const App = () => {
     const dispatch = useDispatch();
-    const location = useLocation();
     const loading = useSelector(isLoading(types.GET_USER));
 
     useEffect(() => {
       dispatch(getUser());
     }, []);
-
-    useEffect(() => {
-      dispatch(setCurrentRoute(location));
-    }, [location]);
 
     return (
       loading
--- src/layout/index.js
+++ src/layout/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useLocation } from 'react-router';
 
+import { setCurrentRoute } from '../siteMap/slice'; 
 import ModulesContainer from './modulesContainer';
 
 const Layout = () => {
+    const dispatch = useDispatch();
+    const location = useLocation();
+
+    useEffect(() => {
+      dispatch(setCurrentRoute(location));
+    }, [location]);
+
     return (
       <ModulesContainer />
     );
