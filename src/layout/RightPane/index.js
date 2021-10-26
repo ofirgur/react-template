3d63b3526eb30895825fdb81e208c682a1d5cc49
Ofir <ofirgu@payoneer.com>
2021-10-26 14:43:35 +0300

    update

---
+++ src/layout/RightPane/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RightPane = () => {
+    return (
+        <div>RightPane</div>
+    );
+};
+
+export default RightPane;
---
+++ src/navigations/MyHoldings/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MyHoldings = () => {
+    return (
+        <div>MyHoldings</div>
+    );
+};
+
+export default MyHoldings;
